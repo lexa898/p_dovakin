7fa33d64865dd7427acd34ad15de2a78540149f1
Анатолий Бурбо < >
2016-12-16 22:47:43 +0700

    Pravka koda nashel kosiak

--- customrandom.h
+++ customrandom.h
@@ -8,31 +8,36 @@ int zagadannoe_chislo [4];
 void Random()
 {
 	srand(time(NULL));
-	for (int i=0; i<4; i++) {   // cikl zapolneniya Randomom
+	for (int i=0; i<4; i++) 
+	{   // cikl zapolneniya Randomom
 	
 		int rand_zapolnenie = 0 + rand() %9;  // Random 
 		int check=0;
 		
-		while (check==0) {
+		while (check==0) 
+		{
 			int count=0;
 			
-			for (int j=0; j<i; j++) {
+			for (int j=0; j<i; j++) 
+			{
 				if (rand_zapolnenie!=zagadannoe_chislo [j])
 			    	count++;
 			}	
 			
-			if (count == i) {
+			if (count == i) 
+			{
 			  zagadannoe_chislo [i] = rand_zapolnenie ;
 			  check=1;
 			}
-			else {
+			else 
+			{
 				rand_zapolnenie = 0 + rand() %9;  // Random again
 			}
 		}
 		
 	
 	
-	 // printf ("%d " ,  zagadannoe_chislo [i] ) ;	 - Это строка для теста что все числа разные. Алгоритм работает нормально
+	  printf ("%d " ,  zagadannoe_chislo [i] ) ;	 // Это строка для теста что все числа разные. Алгоритм работает нормально
 		
 	}
 		
--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ while (1)
 int a1=0,a2=0,a3=0,a4=0;
 printf ("ќтгадайте задуманное число , введите цифры от 0 до 9 **** \n");
 
+int repeat=0;
 
 int chek=0; 
 while (chek==0)
@@ -30,33 +31,37 @@ while (chek==0)
 printf ("¬ведите цифры от 0 до 9\n");
 scanf ("%d%d%d%d",&a1,&a2,&a3,&a4);
 
+
+printf ("\n");
+
+repeat++;
 int sum=0;  // блок кода , проверка на быков и коровок
 int sum2=0;
 
-	if (a1==zagadannoe_chislo[1]) 
+	if (a1==zagadannoe_chislo[0]) 
 	   sum++;
-	if  (a2==zagadannoe_chislo[2]) 
+	if  (a2==zagadannoe_chislo[1]) 
 	   sum++;
-    if  (a3==zagadannoe_chislo[3]) 
+    if  (a3==zagadannoe_chislo[2]) 
 	   sum++;   
-    if  (a4==zagadannoe_chislo[4]) 
+    if  (a4==zagadannoe_chislo[3]) 
 	   sum++; 
 	if (sum==4)
     	{
-	     printf ("вы отгадали");
+	     printf ("вы отгадали, число попыток %d", repeat);
          chek=1;
          getch();
         }
 	else
 	printf ("\n%d быка\n", sum);
 	
-    if(a2== zagadannoe_chislo[1]||a3== zagadannoe_chislo[1]||a4 == zagadannoe_chislo[1])
+    if(a2== zagadannoe_chislo[0]||a3== zagadannoe_chislo[0]||a4 == zagadannoe_chislo[0])
      	sum2++;
-    if(a1== zagadannoe_chislo[2]||a3== zagadannoe_chislo[2]||a4 == zagadannoe_chislo[2]) 
+    if(a1== zagadannoe_chislo[1]||a3== zagadannoe_chislo[1]||a4 == zagadannoe_chislo[1]) 
      	sum2++;
-    if(a1== zagadannoe_chislo[3]||a2== zagadannoe_chislo[3]||a4 == zagadannoe_chislo[3])
+    if(a1== zagadannoe_chislo[2]||a2== zagadannoe_chislo[2]||a4 == zagadannoe_chislo[2])
      	sum2++;
-    if(a1== zagadannoe_chislo[4]||a2== zagadannoe_chislo[4]||a3== zagadannoe_chislo[4])
+    if(a1== zagadannoe_chislo[3]||a2== zagadannoe_chislo[3]||a3== zagadannoe_chislo[3])
      	sum2++;
      	
 		 if (chek==0)		
@@ -75,7 +80,8 @@ break;
 	  getch(); break;  }  
       case '3': return 0;
       default : puts(" неверный режим");
-     }}
+    	 }
+ }
  
   getch();
 return 0;
